4cc4298b6ce52914fe37bdac5d3f128b06e5f6d1
Cam <camiloscamp@gmail.com>
2017-09-24 22:36:23 -0300

    Add own event emitter to avoid making 'CustomEvent' allocations on each frame

--- src/vuni.js
+++ src/vuni.js
@@ -1,76 +1,80 @@
 const Vuni = {
   createGame: function(width, height) {
-    const canvas = document.createElement("canvas");
-    canvas["id"] = "vuniroot";
-    canvas["width"] = width;
-    canvas["height"] = height;
+    const canvas = document.createElement('canvas');
+    canvas['id'] = 'vuniroot';
+    canvas['width'] = width;
+    canvas['height'] = height;
     document.body.appendChild(canvas);
     const ctx = canvas.getContext`2d`;
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, width, height);
-    this.subs = {};
     this.assets = { image: {}, audio: {} };
-    this.listener = canvas.addEventListener.bind(canvas);
-    const listen = evt => (this.subs[evt.type] || (() => {}))(evt.detail);
-    ["update", "render", "keypress", "keyrelease", "onloaded"].forEach(evtKey =>
-      this.listener(evtKey, listen)
+    this.emmiter = {
+      events: {},
+      emit: (evt, data) =>
+        this.emmiter.events[evt].subscribers.forEach(sub => sub(data))
+    };
+    ['update', 'render', 'keypress', 'keyrelease', 'onloaded'].forEach(
+      evtKey => (this.emmiter.events[evtKey] = { subscribers: [] })
     );
     this.on = this.on.bind(this);
     let lastFrameTime = 0;
-    const gameLoop = (ts = 0) => {
+    this.gameLoop = (ts = 0) => {
       if (ts < lastFrameTime + 33.3) {
-        requestAnimationFrame(gameLoop);
+        requestAnimationFrame(this.gameLoop);
         return;
       }
       const dt = ts - lastFrameTime;
       lastFrameTime = ts;
-      canvas.dispatchEvent(new CustomEvent("update", { detail: dt }));
-      canvas.dispatchEvent(new CustomEvent("render"));
-      requestAnimationFrame(gameLoop);
+      this.emmiter.emit('update', dt);
+      this.emmiter.emit('render');
+      requestAnimationFrame(this.gameLoop);
     };
     canvas.tabIndex = 1000;
-    canvas.style.outline = "none";
+    canvas.style.outline = 'none';
     const keyEvent = evtName => (evt = window.event) => {
       evt.preventDefault();
-      canvas.dispatchEvent(
-        new CustomEvent(evtName, { detail: evt.keyCode || 0 })
-      );
+      this.emmiter.emit(evtName, evt.keyCode || 0);
     };
-    canvas.onkeydown = keyEvent("keypress");
-    canvas.onkeyup = keyEvent("keyrelease");
-    this.canvas = canvas;
+    canvas.onkeydown = keyEvent('keypress');
+    canvas.onkeyup = keyEvent('keyrelease');
     return this;
   },
   on: function(event, callback) {
-    this.subs[event] = callback;
+    if (this.emmiter.events[event])
+      this.emmiter.events[event].subscribers.push(callback);
   },
   load: function(assets) {
     const loader = assets.map(assetDef => {
       return new Promise((res, rej) => {
-        const asset = assetDef.type === "image" ? new Image() : new Audio();
+        const asset = assetDef.type === 'image' ? new Image() : new Audio();
         asset.onerror = rej;
-        asset[assetDef.type === "image" ? "onload" : "oncanplaythrough"] = res;
+        asset[assetDef.type === 'image' ? 'onload' : 'oncanplaythrough'] = res;
         asset.src = assetDef.src;
         this.assets[assetDef.type][assetDef.id] = asset;
       });
     });
     Promise.all(loader).then(_ => {
-      this.canvas.dispatchEvent(new CustomEvent("onloaded"));
+      this.emmiter.emit('onloaded');
+      this.gameLoop();
     });
-  },
-  start: () => gameLoop()
+  }
 };
 
 // Tests ->
 
 Vuni.createGame(400, 300).load([
-  { id: "mother", type: "image", src: "mother3.png" },
-  { id: "mother2", type: "image", src: "mother3.png" }
+  { id: 'mother', type: 'image', src: 'mother3.png' },
+  { id: 'mother2', type: 'image', src: 'mother3.png' }
 ]);
 
-Vuni.on("onloaded", () => {
-  console.log("Resources loaded");
+Vuni.on('onloaded', () => {
+  console.log('Resources loaded');
   console.log(Vuni.assets);
+});
+
+Vuni.on('keyrelease', keycode => {
+  console.log(keycode);
 });
 
 export default Vuni;
