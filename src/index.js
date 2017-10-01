5ff8a389b11cc29a25d7dae06a56a1003b173895
Cam <c4miloei@gmail.com>
2017-10-01 00:35:03 -0700

    Change in the way modules are exported

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-export * from './core/shyer';
-export * from './extensions/audio';
-export * from './extensions/sprite';
-export * from './extensions/camera';
+import * as Shyer from './core/shyer';
+import * as Audio from './extensions/audio';
+import * as Sprite from './extensions/sprite';
+import * as Camera from './extensions/camera';
+
+export { Shyer, Audio, Sprite, Camera };
