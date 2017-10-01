bf1725f0949ba37cf11e6bf90dfa1a771ec39760
Cam <c4miloei@gmail.com>
2017-10-01 00:34:03 -0700

    Fix sprite collision and animation

--- src/extensions/sprite.js
+++ src/extensions/sprite.js
@@ -1,24 +1,9 @@
-export function createSprite(
-  id,
-  resId,
-  x,
-  y,
-  width,
-  height,
-  speed = 0,
-  visible = true,
-  collidable = false,
-  isStatic = false
-) {
-  let colliding = false;
+export function createSprite(id, resId, x, y, width, height) {
+  let speed = 0;
+  let visible = true;
+  let isStatic = false;
 
-  function update(dt) {
-    colliding = collidable ? false : checkCollidde();
-  }
-
-  function checkCollidde() {
-    return false; // placeholder
-  }
+  function update(dt) {}
 
   return {
     id,
@@ -30,9 +15,21 @@ export function createSprite(
     speed,
     visible,
     isStatic,
-    colliding,
     update
   };
+}
+
+export function collideWith(source, target, onCollideFn, enablerExp = true) {
+  if (!enablerExp) return;
+
+  const { x, y, width, height } = source;
+  const { x: x2, y: y2, width: width2, height: height2 } = target;
+  const collisionResult =
+    x < x2 + width2 && x + width > x2 && y < y2 + height2 && y + height > y2;
+
+  if (collisionResult) {
+    onCollideFn();
+  }
 }
 
 export function renderSprite(
@@ -50,13 +47,17 @@ export function renderGroup(sprites = [], ctx, cache, renderFn) {
   sprites.forEach(sprite => renderFn(sprite, ctx, cache));
 }
 
-export function createAnimatedSprite(sprite, crop, clips, startingClip = "") {
+export function createAnimatedSprite(sprite, crop, clips, startingClip = '') {
   let currentClip = startingClip;
   const { rows, columns, cropSize } = crop;
   const { update: parentUpdate, speed } = sprite;
 
   function setCurrentClip(clipName) {
-    currentClip = clipName;
+    if (clips[clipName]) {
+      currentClip = clipName;
+      clips[clipName].idx = clips[clipName].origin;
+      clips[clipName].time = 0;
+    }
   }
 
   function getCurrentClip() {
@@ -65,14 +66,18 @@ export function createAnimatedSprite(sprite, crop, clips, startingClip = "") {
 
   function update(dt) {
     parentUpdate(dt);
+
     if (!clips) return;
+
     if (!getCurrentClip()) return;
+
     const clip = getCurrentClip();
     clip.time += dt * speed;
+
     if (clip.time > clip.duration) {
       clip.time = 0;
       clip.idx += 1;
-      if (clip.idx > clip.origin + clip.length) {
+      if (clip.idx >= clip.origin + clip.length) {
         clip.idx = clip.origin;
       }
     }
@@ -95,7 +100,7 @@ export function createAnimationClip(tileOrigin, tilesLength, secondsPerFrame) {
     length: tilesLength,
     duration: secondsPerFrame * 100,
     time: 0,
-    idx: 0
+    idx: tileOrigin
   };
 }
 
